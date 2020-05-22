f5eeda7b87e89a6a1e6c797a97329e018e18e389
Samsul Hadi <samsulhadikediri@gmail.com>
2020-05-22 20:52:07 +0700

    Initial Commit

---
+++ Interface Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Interface Builder
+//
+//  Created by SAMSUL HADI on 22/05/20.
+//  Copyright © 2020 SAMSUL HADI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Interface Builder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Interface Builder
+//
+//  Created by SAMSUL HADI on 22/05/20.
+//  Copyright © 2020 SAMSUL HADI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
